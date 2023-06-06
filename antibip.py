82b77fba7dc6da0574ab123d0e2f7b2f9fee14be
Hung Nguyen <101444336+hungnguyennaz@users.noreply.github.com>
2023-06-06 19:33:09 +0700

    Dùng ipset cho đỡ nặng, clean code

--- antibip.py
+++ antibip.py
@@ -2,6 +2,14 @@ import subprocess
 import requests
 
 
+def check_if_ipset_installed():
+    try:
+        subprocess.check_output(["sudo", "ipset", "list"], stderr=subprocess.DEVNULL)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
 def check_if_iptables_installed():
     try:
         subprocess.check_output(["sudo", "iptables", "-L"], stderr=subprocess.DEVNULL)
@@ -10,8 +18,37 @@ def check_if_iptables_installed():
         return False
 
 
+<<<<<<< HEAD
+def create_ipset_blacklist():
+    subprocess.run(
+        ["sudo", "ipset", "create", "antibip-blacklist", "hash:ip", "hashsize", "8192"]
+    )
+
+
+def add_ip_to_blacklist(ip):
+    subprocess.run(["sudo", "ipset", "add", "antibip-blacklist", ip])
+
+
+def block_ipset_blacklist():
+    subprocess.run(
+        [
+            "sudo",
+            "iptables",
+            "-I",
+            "INPUT",
+            "-m",
+            "set",
+            "--match-set",
+            "antibip-blacklist",
+            "src",
+            "-j",
+            "DROP",
+        ]
+    )
+=======
 def block_ip(ip):
     subprocess.run(["sudo", "iptables", "-I", "INPUT", "-s", ip, "-j", "DROP"])
+>>>>>>> 655e8e2982f3f3544b05ec20e9703e16169d1b7d
 
 
 def get_scanners_ips():
@@ -40,10 +77,15 @@ def block_scanners_ips():
         print("Đã thoát.")
         return
 
+    print("Hãy đợi chút... (có thể mất từ 1-2 phút)")
     scanners_ips = get_scanners_ips()
 
+    create_ipset_blacklist()
+
     for ip in scanners_ips:
-        block_ip(ip)
+        add_ip_to_blacklist(ip)
+
+    block_ipset_blacklist()
 
     print("")
     print("Đã chặn thành công các địa chỉ IP từ danh sách.")
@@ -51,18 +93,54 @@ def block_scanners_ips():
     print("")
 
 
+def uninstall_antibip():
+    confirmation = input("Bạn có chắc chắn muốn gỡ bỏ antibip không? (y/n): ")
+
+    if confirmation.lower() not in ["y", "yes"]:
+        print("Đã thoát.")
+        return
+
+    subprocess.run(
+        [
+            "sudo",
+            "iptables",
+            "-D",
+            "INPUT",
+            "-m",
+            "set",
+            "--match-set",
+            "antibip-blacklist",
+            "src",
+            "-j",
+            "DROP",
+        ]
+    )
+    subprocess.run(["sudo", "ipset", "flush", "antibip-blacklist"])
+    subprocess.run(["sudo", "ipset", "destroy", "antibip-blacklist"])
+    subprocess.run(["sudo", "ipset", "destroy", "antibip-blacklist"])
+
+    print("Đã gỡ bỏ antibip.")
+    print("")
+
+
 def main():
+    if not check_if_ipset_installed():
+        print(
+            "**ipset** chưa được cài đặt. Vui lòng cài đặt **ipset** và chạy lại script."
+        )
+        return
+
     if not check_if_iptables_installed():
         print(
-            "IPTables chưa được cài đặt. Vui lòng cài đặt IPTables và chạy lại script."
+            "**iptables** chưa được cài đặt. Vui lòng cài đặt **iptables** và chạy lại script."
         )
         return
 
     print(
-        "Cảnh báo: Script này sẽ giúp các bạn có thể hạn chế được một số công cụ quét mạng như Censys/Shodan (Censys và Shodan là công cụ tìm kiếm và quét mạng trực tuyến. Chúng giúp thu thập thông tin về các thiết bị kết nối với Internet. Tuy nhiên, khi được sử dụng bởi kẻ xấu, chúng có thể tìm lỗ hổng bảo mật và thông tin nhạy cảm để tấn công hoặc xâm nhập vào hệ thống). Bằng cách tải một danh sách các địa chỉ IPs của các scanners đã được tìm thấy và chặn chúng bằng IPTables."
+        "Cảnh báo: Script này sẽ giúp bạn có thể hạn chế được một số công cụ quét mạng như Censys/Shodan (Censys và Shodan là công cụ tìm kiếm và quét mạng trực tuyến. Chúng giúp thu thập thông tin về các thiết bị kết nối với Internet. Tuy nhiên, khi được sử dụng bởi kẻ xấu, chúng có thể tìm lỗ hổng bảo mật và thông tin nhạy cảm để tấn công hoặc xâm nhập vào hệ thống). Bằng cách tải một danh sách các địa chỉ IPs của các scanners đã được tìm thấy và chặn chúng bằng iptables và ipset."
     )
     print(
-        "Tuy nhiên, do script sử dụng IPTables, bạn hãy chắc chắn rằng mình đã có một bản backup rules IPTables để có thể revert lại nếu như script gây ra trục trặc. Cũng như, hãy đảm bảo rằng bạn đã cài đặt IPTables và requests (pip install requests/pip3 install requests) để có thể sử dụng."
+        "Tuy nhiên, do script sử dụng iptables và ipset, bạn hãy chắc chắn rằng mình đã có một bản backup rules iptables để có thể revert lại nếu như script gây ra trục trặc. Cũng như, hãy đảm bảo rằng bạn đã cài đặt iptables, ipset và requests (pip install requests/pip3 install requests) để có thể sử dụng."
     )
     print("")
     print("Brought to you by hungaz")
@@ -77,6 +155,7 @@ def main():
     print("--------------- MENU --------------")
     print(" ")
     print("1. Chặn Censys/Shodan/Một số scanners khác")
+    print("2. Gỡ bỏ antibip")
     print(" ")
     print("-----------------------------------")
     print(" ")
@@ -84,8 +163,10 @@ def main():
 
     if choice == "1":
         block_scanners_ips()
+    elif choice == "2":
+        uninstall_antibip()
     else:
-        print("Lựa chọn của bạn không hợp lệ.")
+        print("Lựa chọn không hợp lệ.")
 
 
 if __name__ == "__main__":
